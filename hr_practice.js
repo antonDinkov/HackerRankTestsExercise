14f182fdf9928aa1e91fec6937cee4d750f74b40
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-08-18 20:31:41 +0300

    timeConversion

--- hr_practice.js
+++ hr_practice.js
@@ -48,3 +48,35 @@ function birthdayCakeCandles(candles) {
     })
     return countTallests;
 }
+
+
+function timeConversion(s) {
+    // Write your code here
+    let amOrPm = s.slice(-2);
+    let mutatedS = s.slice(0, 8);
+    let convertedToArr = mutatedS.split(':');
+    let hours = convertedToArr.shift();
+    const timeList = {
+        "01": "13",
+        "02": "14",
+        "03": "15",
+        "04": "16",
+        "05": "17",
+        "06": "18",
+        "07": "19",
+        "08": "20",
+        "09": "21",
+        "10": "22",
+        "11": "23",
+        "12": "00"
+    }
+    if ((amOrPm == 'PM' && hours != '12') || (amOrPm == 'AM' && hours == '12')) {
+        let replacement = timeList[hours];
+        convertedToArr.unshift(replacement)
+    } else {
+        convertedToArr.unshift(hours);
+    }
+    
+    const formattedTime = convertedToArr.join(':');
+    return formattedTime;
+}
