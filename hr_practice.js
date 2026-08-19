e58e5a39b3ac809dc97293fffde331faf00fd756
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-08-19 11:49:29 +0300

    weightedUniformStrings

--- hr_practice.js
+++ hr_practice.js
@@ -117,3 +117,70 @@ function miniMaxSum(arr) {
 
     console.log(sum - max, sum - min);
 }
+
+function weightedUniformStrings(s, queries) {
+    // Write your code here
+    const alphabetWeights = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26
+    }
+    const result = [];
+    const weights = [];
+    let uniform = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        let weightToPush;
+        if (i == 0) {
+            weightToPush = alphabetWeights[s[i]];
+            weights.push(weightToPush);
+            uniform = s[i];
+            continue;
+        };
+        if (s[i] == s[i-1]) {
+            uniform += s[i];
+            weightToPush = alphabetWeights[s[i]] * uniform.length;
+            weights.push(weightToPush);
+        } else {
+            weightToPush = alphabetWeights[s[i]];
+            weights.push(weightToPush);
+            uniform = s[i];
+        }
+    }
+    
+    for (let q of queries) {
+        if (weights.includes(q)) {
+            result.push('Yes');
+        } else {
+            result.push('No');
+        };
+    };
+    
+    return result;
+}
+
+console.log(weightedUniformStrings('abbcccdddd', [1, 7, 5, 4, 15]));
+console.log(weightedUniformStrings('abccddde', [1, 3, 12, 5, 9, 10]));
