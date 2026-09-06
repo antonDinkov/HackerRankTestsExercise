49a3b0f1a81a4ac310a1151dc3c7a175e8dbb68d
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-09-06 19:33:34 +0300

    formingMagicSquare

--- hr_practice.js
+++ hr_practice.js
@@ -516,3 +516,33 @@ function getMoneySpent(keyboards, drives, b) {
         return biggestPriceOfBoth;
     };
 }
+
+function formingMagicSquare(s) {
+    // Write your code here
+    const magicSquares = [
+        [8, 1, 6, 3, 5, 7, 4, 9, 2],
+        [6, 1, 8, 7, 5, 3, 2, 9, 4],
+        [4, 9, 2, 3, 5, 7, 8, 1, 6],
+        [2, 9, 4, 7, 5, 3, 6, 1, 8],
+        [8, 3, 4, 1, 5, 9, 6, 7, 2],
+        [4, 3, 8, 9, 5, 1, 2, 7, 6],
+        [6, 7, 2, 1, 5, 9, 8, 3, 4],
+        [2, 7, 6, 9, 5, 1, 4, 3, 8]
+    ];
+
+    const input = s.flat();
+    let minCost = Infinity;
+
+    for (const magic of magicSquares) {
+        let cost = 0;
+
+        for (let i = 0; i < 9; i++) {
+            cost += Math.abs(input[i] - magic[i]);
+        }
+
+        minCost = Math.min(minCost, cost);
+    }
+
+    return minCost;
+
+}
